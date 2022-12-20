579c8d51809c8418cab472c003c191f9affb0e42
Warcen11 <warcen2006@gmail.com>
2022-12-20 18:51:08 +0400

    tripList

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config({ path: './token.env' });
-
+const axios = require('axios').default
 const token=process.env.TOKEN;
 
 const bot = new TelegramBot(token, {polling: true});
 
-const chatId = msg.chat.id;
+axios.get('http://localhost:3030/trips/get-all').then(response =>{
+    let inline_keyboard = []
+    for(let i = 0; i<response.data.length; i++){
+        inline_keyboard[i] = [{text: response.data[i].name, callback_data: response.data[i]._id}]
+    }
+    tripInfo = {
+        reply_markup: JSON.stringify({
+            inline_keyboard
+        })
+    }
+})
+
+
 
 bot.setMyCommands(
     [
         {command: '/start', description: 'Приветствие'},
+        {command: '/trips', description: 'Узнать о своих поездках'},
     ]
 )
 
 bot.on('message', async msg => {
+    chatId = msg.chat.id
     if(msg.text == '/start'){
         return bot.sendMessage(chatId, 'Привет, я буду отправлять сюда информацию о ваших поездках');
     }
+    if(msg.text == '/trips'){
+        return bot.sendMessage(chatId, 'Ваши поездки:', tripInfo)
     return bot.sendMessage(chatId, 'Простите, но я не понял что вы сказали')
-  });
+    }
+})
