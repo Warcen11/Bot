0c9e7d356268b347301c89d9fc793cef5338e1dc
Warcen11 <warcen2006@gmail.com>
2023-04-12 12:19:24 +0400

    confident

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config({ path: './token.env' });
 const axios = require('axios').default
 const token=process.env.TOKEN;
+const mychatid=process.env.CHATID
 
 const bot = new TelegramBot(token, {polling: true});
 
 bot.setMyCommands(
     [
-        {command: '/start', description: 'Приветствие'},
+        {command: '/start', description: 'start'},
     ]
 )
 
@@ -15,12 +16,6 @@ bot.on('message', async msg => {
     chatId = msg.chat.id
     if(msg.text == '/start'){
         return bot.sendMessage(chatId, 'Привет');
-    }
-    if(msg.text == '/trips'){
-        return getTripList().then(tripList => {
-            bot.sendMessage(chatId, 'Ваши поездки :', tripList)
-        })
-    return bot.sendMessage(chatId, 'Простите, но я не понял что вы сказали')
     }
 })
 
@@ -32,24 +27,23 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     origin: ["http://localhost:5173"],
-    // credentials: true
 }))
 
 app.post('/create-trip', function (req) {
-    bot.sendMessage(660411344, `Поездка создана:
+    bot.sendMessage(mychatid, `Поездка создана:
 http://localhost:5173/trip?_id=${req.query._id}`)
 })
 app.post('/create-user', function (req){
-    bot.sendMessage(660411344, `Зарегистрирован пользователь:
+    bot.sendMessage(mychatid, `Зарегистрирован пользователь:
 ${req.query._fullname}
 ${req.query._email}`)
 })
 app.post('/add-companion', function (req) {
-    bot.sendMessage(660411344, `Добавлен попутчик:
+    bot.sendMessage(mychatid, `Добавлен попутчик:
 ${req.query.name}`)
 })
 app.post('/add-guide', function (req) {
-    bot.sendMessage(660411344, `Добавлен элемент гида:
+    bot.sendMessage(mychatid, `Добавлен элемент гида:
 ${req.query.name}`)
 })
 
@@ -58,5 +52,5 @@ try {
         console.log(`Server is running on http://localhost:4089`);
     })
 } catch (error) {
-    console.log('у тебя ошибка');
+    console.log(error);
 }
